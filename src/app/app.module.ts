d255b9c8dc2ffe5bc672e9cdd9075c19096e3d24
Naveen Kumar <naveen.kumar@mgazo.com>
2023-07-01 22:43:59 +0530

    login, reset password, create account UX updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StaticIconsDialog, SetupComponent } from './pages/setup/setup.component';
 import { DashboardComponent, SetupTopSnackbar } from './pages/dashboard/dashboard.component';
+import { CustomerFeedbackComponent } from './pages/customer-feedback/customer-feedback.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SetupComponent, StaticIconsDialog, SetupTopSnackbar, DashboardComponent],
+  declarations: [AppComponent, LoginComponent, SetupComponent, StaticIconsDialog, SetupTopSnackbar, DashboardComponent, CustomerFeedbackComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,9 +8,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  agreementChecked = false;
+  enableForms: any = {
+    isLoginForm: true,
+    isForgotPasswordForm: false,
+    isRegisterForm: false,
+  };
+
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
+  });
+
+  forgetPasswordForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
+  registerForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required]),
+    companyName: new FormControl('', [Validators.required]),
+    registerEmail: new FormControl('', [Validators.required, Validators.email]),
+    registerPassword: new FormControl('', [Validators.required]),
+    registerRepeatPassword: new FormControl('', [Validators.required]),
   });
 
   get email(): any {
@@ -19,6 +38,30 @@ export class LoginComponent implements OnInit {
 
   get password(): any {
     return this.loginForm.get('password');
+  }
+
+  get forgotPasswordEmail(): any {
+    return this.forgetPasswordForm.get('email');
+  }
+
+  get fullName(): any {
+    return this.registerForm.get('fullName');
+  }
+
+  get companyName(): any {
+    return this.registerForm.get('companyName');
+  }
+
+  get registerEmail(): any {
+    return this.registerForm.get('registerEmail');
+  }
+
+  get registerPassword(): any {
+    return this.registerForm.get('registerPassword');
+  }
+
+  get registerRepeatPassword(): any {
+    return this.registerForm.get('registerRepeatPassword');
   }
 
   constructor(private router: Router) {}
@@ -30,4 +73,14 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  enableFormHandler(formType: string): void {
+    for (let cForm in this.enableForms) {
+      if (cForm === formType) {
+        this.enableForms[formType] = true;
+      } else {
+        this.enableForms[cForm] = false;
+      }
+    }
+  }
 }
